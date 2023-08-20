f8e33250563594b52eaab3736eeadf69ab1bdeeb
edidiesky <essienedidiong1000@gmail.com>
2023-08-20 12:32:34 -0700

    new fixes

--- frontend/src/components/common/Rating.js
+++ frontend/src/components/common/Rating.js
@@ -1,33 +1,34 @@
-import React from 'react'
-import Styled from 'styled-components/macro'
-import {FaStarHalfAlt, FaStar} from 'react-icons/fa'
-import {BsStar, BsStarFill} from 'react-icons/bs'
+import React from "react";
+import Styled from "styled-components/macro";
+import { FaStarHalfAlt, FaStar } from "react-icons/fa";
+import { BsStar, BsStarFill } from "react-icons/bs";
 
+export default function Rating({ value, type }) {
+  // body...
+  // if(type === 'review') {
+  // 	return <RatingContent>
+  //         <div className='RatingContainer'>
+  //           <span>{value >= 1 ? <BsStarFill/>: value< 0.5? <BsStar/> : <BsStarFill/> }</span>
+  //           <span>{value >= 2 ? <BsStarFill/>: value<1.5? <BsStar/> : <BsStarFill/> }</span>
+  //           <span>{value >= 3 ? <BsStarFill/>: value<2.5? <BsStar/> : <BsStarFill/> }</span>
+  //           <span>{value >= 4 ? <BsStarFill/>: value<3.5? <BsStar/> : <BsStarFill/> }</span>
+  //           <span>{value >= 5 ? <BsStarFill/>: value<4.5? <BsStar/> : <BsStarFill/> }</span>
 
-export default function Rating({value, type}) {
-	// body...
-	if(type ==='review') {
-		return <RatingContent>
-	        <div className='RatingContainer'>
-	          <span>{value >= 1 ? <BsStarFill/>: value< 0.5? <BsStar/> : <BsStarFill/> }</span>
-	          <span>{value >= 2 ? <BsStarFill/>: value<1.5? <BsStar/> : <BsStarFill/> }</span>
-	          <span>{value >= 3 ? <BsStarFill/>: value<2.5? <BsStar/> : <BsStarFill/> }</span>
-	          <span>{value >= 4 ? <BsStarFill/>: value<3.5? <BsStar/> : <BsStarFill/> }</span>
-	          <span>{value >= 5 ? <BsStarFill/>: value<4.5? <BsStar/> : <BsStarFill/> }</span>
-	          
-	        </div>
-	       </RatingContent>
-	}
-	return <RatingContent>
-	        <div className='RatingContainer'>
-	          <span>{value >= 1 ? <FaStar/>: value< 0.5? <FaStarHalfAlt/> : <FaStar/> }</span>
+  //         </div>
+  //        </RatingContent>
+  // }
+  return (
+    <RatingContent>
+      <div className="RatingContainer">
+        {/* <span>{value >= 1 ? <FaStar/>: value< 0.5? <FaStarHalfAlt/> : <FaStar/> }</span>
 	          <span>{value >= 2 ? <FaStar/>: value<1.5? <FaStarHalfAlt/> : <FaStar/> }</span>
 	          <span>{value >= 3 ? <FaStar/>: value<2.5? <FaStarHalfAlt/> : <FaStar/> }</span>
 	          <span>{value >= 4 ? <FaStar/>: value<3.5? <FaStarHalfAlt/> : <FaStar/> }</span>
 	          <span>{value >= 5 ? <FaStar/>: value<4.5? <FaStarHalfAlt/> : <FaStar/> }</span>
-	          
-	        </div>
-	       </RatingContent>
+	           */}
+      </div>
+    </RatingContent>
+  );
 }
 
 const RatingContent = Styled.div`
@@ -50,4 +51,4 @@ h3 {
 		font-weight:600;
 	}
 
-`
+`;
