a49c78be29be9528b9ecbd174a5d1a5786dcfdbf
edidiesky <essienedidiong1000@gmail.com>
2023-12-12 23:21:30 -0800

    new fixes

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -43,6 +43,8 @@ router.post('/', upload.array('images', 4), (req, res) => {
     return `/${path}`
 
   })
+   res.setHeader("Content-Type", "text/html");
+   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.json({ files })
 })
 
