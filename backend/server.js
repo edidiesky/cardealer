948a5f696f0287f9f99bc14ee00d5b40738c3ec6
edidiesky <essienedidiong1000@gmail.com>
2023-12-13 18:52:38 -0800

    new

--- backend/server.js
+++ backend/server.js
@@ -20,16 +20,16 @@ import mongoose from "mongoose";
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookie());
-import productRoute from "./routes/productRoute.js";
-import userRoute from "./routes/userRoute.js";
+// import productRoute from "./routes/productRoute.js";
+// import userRoute from "./routes/userRoute.js";
 
-import uploadRoute from "./routes/uploadRoute.js";
-import orderRoute from "./routes/orderRoutes.js";
+// import uploadRoute from "./routes/uploadRoute.js";
+// import orderRoute from "./routes/orderRoutes.js";
 
-app.use("/api/v1/product", productRoute);
-app.use("/api/v1/auth", userRoute);
-app.use("/api/v1/order", orderRoute);
-app.use("/api/v1/upload", uploadRoute);
+// app.use("/api/v1/product", productRoute);
+// app.use("/api/v1/auth", userRoute);
+// app.use("/api/v1/order", orderRoute);
+// app.use("/api/v1/upload", uploadRoute);
 const __dirname = path.resolve();
 
 app.use(
